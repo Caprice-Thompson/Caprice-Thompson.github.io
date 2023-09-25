80a81fdaedc0404fd57f60041daf2d952b878a97
Caprice-Thompson <caprice.thompson@itv.com>
2023-09-25 17:15:10 +0100

    cypress tests

---
+++ cypress/e2e/home-spec.cy.js
@@ -0,0 +1,27 @@
+// export const getByAriaLabel = element.getAttribute('aria-label'); 
+// cypress/integration/sample-test.spec.js
+describe('Home page', () => {
+  beforeEach(() => {
+    // This code will run before each test case
+    cy.visit('Caprice-Thompson.github.io'); // Replace with your website URL
+  });
+  it('displays page title', () => {
+    // Your test case code here
+    cy.get('a[aria-label="homeHeader"]').contains('Wiki Treats'); // Replace with an element on your webpage
+  });
+
+  // it('Test Case 2', () => {
+  //   // Your test case code here
+  //   cy.get('input[type="text"]').type('Cypress Test'); // Example interaction with an input field
+  // });
+
+  
+
+});
+// describe('Home Page', () => {
+//     it('Visits the homepage', () => {
+//       cy.visit('Caprice-Thompson.github.io'); // Replace with your website URL
+//       cy.contains('Wiki Treats'); // Replace with an element on your webpage
+//     });
+//   });
+  
