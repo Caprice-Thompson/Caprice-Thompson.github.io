fe00367072c018da687d29e385232da1b9a28ab9
Caprice-Thompson <caprice.thompson@itv.com>
2023-10-21 00:34:14 +0100

    wip

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,4 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+export const getLabel = (attribute) => cy.get(`[aria-label="${attribute}"]`);
