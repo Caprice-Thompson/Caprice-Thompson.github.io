9fade6f490af133cbb15bc6b42df36a3854754aa
Caprice-Thompson <caprice.thompson@itv.com>
2023-10-22 20:24:00 +0100

    wip

--- cypress/support/step_definitions/ServicesPageSteps.js
+++ cypress/support/step_definitions/ServicesPageSteps.js
@@ -2,22 +2,13 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { getLabel } from '../commands';
 
 And('I navigate to the Services page', () => {
-  cy.visit('caprice-thompson.github.io/services.html');
-    // getLabel('pricingPageTab').should('be.visible').click();
+  cy.visit('http://127.0.0.1:5500/services.html');
   });
 
 Then('I am presented with the Services Page', () => {
   getLabel('servicesPage').should('be.visible');
   getLabel('servicesPage').should('contain.text','Services');
 });
-// // Buy House Now! Limited Time Only
-// Then('I am on the listing modal', (dataTable) => {
-//   const title = dataTable.rawTable[0][0];
-//   getLabel('listingTitle').should('be.visible');
-//   getLabel('listingTitle').should('contain.text', title);
-//   // cy.get('.imgHere').should('exist').and('be.visible');
-//   // cy.get('.imgHere').should('include', 'images/page4-img2.jpg');
-// });
 
 Then('I am viewing Service Offers', () => {
   getLabel('offerLabels').should('be.visible');
